eed5b3c842a7bf07a1953bc97ccbd4b31afe132f
Joey <31974730+Joeysantoro@users.noreply.github.com>
2023-11-13 12:02:29 -0500

    Update IERC7540.sol

--- src/interfaces/IERC7540.sol
+++ src/interfaces/IERC7540.sol
@@ -5,16 +5,16 @@ import {IERC4626} from "./IERC4626.sol";
 
 interface IERC7540 is IERC4626 {
 
-    event Request(address indexed sender, address indexed operator, uint8 indexed RequestType, uint256 assets, uint256 shares);
+    event Request(address indexed sender, address indexed receiver, uint8 indexed RequestType, uint256 assets, uint256 shares);
 
     // Required
     function ownerOf(uint256 rid);
 
-    function claimRequest(uint256 rid, address to, RequestType requestType);
+    function claimRequest(uint256 rid, address receiver, RequestType requestType);
 
-    function transferRequest(uint256 rid, address to, RequestType requestType);
+    function transferRequest(uint256 rid, address receiver, RequestType requestType);
 
-    function request(uint256 assets, uint256 shares, address operator, RequestType requestType) external returns (uint256 rid);
+    function request(uint256 assets, uint256 shares, address receiver, RequestType requestType) external returns (uint256 rid);
 
     function viewRequest(uint256 rid, RequestType requestType) external view returns (uint256 pendingAssets, uint256 pendingShares, uint256 claimableAssets, uint256 claimableShares);
 
