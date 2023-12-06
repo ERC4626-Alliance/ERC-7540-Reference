59639dad12d056da815481739f81483257d6f7ad
Joey Santoro <joeysantoro111@gmail.com>
2023-12-06 14:17:49 -0500

    test

--- src/ERC7540AsyncDepositExample.sol
+++ src/ERC7540AsyncDepositExample.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.15;
+pragma solidity ^0.8.15;
 
 import "solmate/mixins/ERC4626.sol";
 import "solmate/auth/Owned.sol";
--- test/TestInterfaceFunctionSelectors.t.sol
+++ test/TestInterfaceFunctionSelectors.t.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.15;
 
-import "std/Test.sol";
+import "forge-std/Test.sol";
 import {IERC7540Deposit, IERC7540Redeem} from "src/interfaces/IERC7540.sol";
 
 contract LiquidityPoolTest is Test {
   
 
     // --- erc165 checks ---
-    function testERC165Support(bytes4 unsupportedInterfaceId) public {
+    function testERC165Support( /*bytes4 unsupportedInterfaceId */) public {
         bytes4 erc165 = 0x01ffc9a7;
         bytes4 erc7540Deposit = 0x1683f250;
         bytes4 erc7540Redeem = 0x0899cb0b;
