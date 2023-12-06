91397a8469327a48996bc29a9abbac60f5206f8d
Joey Santoro <joeysantoro111@gmail.com>
2023-12-06 14:17:58 -0500

    test

--- test/TestInterfaceFunctionSelectors.t.sol
+++ test/TestInterfaceFunctionSelectors.t.sol
@@ -8,24 +8,19 @@ contract LiquidityPoolTest is Test {
   
 
     // --- erc165 checks ---
-    function testERC165Support( /*bytes4 unsupportedInterfaceId */) public {
-        bytes4 erc165 = 0x01ffc9a7;
+    function testERC165InterfaceSelectors() public {
         bytes4 erc7540Deposit = 0x1683f250;
         bytes4 erc7540Redeem = 0x0899cb0b;
 
-        // vm.assume(unsupportedInterfaceId != erc165 && unsupportedInterfaceId != erc7540Deposit && unsupportedInterfaceId != erc7540Redeem);
-
-        // address lPool_ = deploySimplePool();
-        // LiquidityPool lPool = LiquidityPool(lPool_);
+        assertEq(type(IERC7540Deposit).interfaceId, erc7540Deposit);
+        assertEq(type(IERC7540Redeem).interfaceId, erc7540Redeem);
+    }
+    
+    function testERC165InterfaceSelectors() public {
+        bytes4 erc7540Deposit = 0x1683f250;
+        bytes4 erc7540Redeem = 0x0899cb0b;
 
         assertEq(type(IERC7540Deposit).interfaceId, erc7540Deposit);
         assertEq(type(IERC7540Redeem).interfaceId, erc7540Redeem);
-
-        // assertEq(lPool.supportsInterface(erc165), true);
-        // assertEq(lPool.supportsInterface(erc7540Deposit), true);
-        // assertEq(lPool.supportsInterface(erc7540Redeem), true);
-
-        // assertEq(lPool.supportsInterface(unsupportedInterfaceId), false);
     }
-    
 }
